05a58d152d71f43f55392202f8f5a567a58ce4fb
netizenrebuild <weister.alex@gmail.com>
2026-08-28 21:28:17 -0700

    Use standalone addon routes in local server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import http from "node:http";
-import { handler } from "./src/app.js";
+import handler from "./api/index.js";
 
 const port = Number(process.env.PORT || 7000);
 http.createServer(handler).listen(port, "0.0.0.0", () => {
-  console.log(`Naruto Rebuild RD Library setup: http://localhost:${port}/configure`);
+  console.log(`Naruto Rebuild Debrid setup: http://localhost:${port}/configure`);
 });
